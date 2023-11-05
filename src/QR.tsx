75672d384d9aaf9a5ac1b026330608f999a51947
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-11-06 00:13:09 +0530

    QR component added with basic styling

---
+++ src/QR.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QR = () => {
+  return (
+    <div>
+      <h1 className="py-6 text-3xl font-bold underline">OR Component</h1>
+    </div>
+  );
+};
+
+export default QR;
