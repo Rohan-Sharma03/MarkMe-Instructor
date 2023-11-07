23c9358036ede34cf23802b36b5902cf7eaf7577
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-11-07 23:11:10 +0530

    Slight desgin Changes were made

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import Home from "./Home";
-import QR from "./OR";
+import QR from "./QR"; // Corrected import
+import NavigationBar from "./Components/NavigationBar";
+import Profile from "./Components/profile";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/qr" element={<QR />} />
-      </Routes>
+      <div>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/qr" element={<QR />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
