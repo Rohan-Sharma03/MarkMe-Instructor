8dc44e294baeeb2b325cd6748e786b4e8fe254ba
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-11-07 23:09:53 +0530

    Updated the sidebar with Standard design

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa"; // Import the icons
+import { FaBars, FaTimes, FaUser } from "react-icons/fa"; // Import the icons
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 export default function NavigationBar() {
   const [showSidebar, setShowSidebar] = useState(false);
-  const sidebarRef = useRef<HTMLDivElement | null>(null); // Adjusted the type assertion
+  const sidebarRef = useRef<HTMLDivElement>(null);
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
 
-  const handleClickOutside = (event: Event) => {
+  const handleClickOutside = (event: MouseEvent) => {
     if (
       sidebarRef.current &&
-      !(sidebarRef.current as any).contains(event.target) // Type assertion adjusted
+      !sidebarRef.current.contains(event.target as Node)
     ) {
       setShowSidebar(false);
     }
@@ -25,27 +26,47 @@ export default function NavigationBar() {
     };
   }, []);
 
+  const getCurrentDate = () => {
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const today = new Date();
+    const formatter = new Intl.DateTimeFormat("en-US", options);
+    return formatter.format(today);
+  };
+
+  const currentDate = getCurrentDate();
+
   return (
-    <div className="flex flex-row justify-between p-3">
-      <div className="flex flex-row space-x-3">
-        <div>
-          <button
-            style={{ background: "none", border: "none" }}
-            onClick={toggleSidebar}
-          >
-            {showSidebar ? <FaTimes size={20} /> : <FaBars size={20} />}
-          </button>
-          Hamburger
-        </div>
-        <div>box 1</div>
+    <div className="fixed top-0 left-0 w-full z-50  flex items-center justify-between p-3 bg-gray-100 shadow">
+      <div className="flex items-center space-x-3">
+        <button
+          style={{ background: "none", border: "none", outline: "none" }}
+          onClick={toggleSidebar}
+        >
+          {showSidebar ? <FaTimes size={20} /> : <FaBars size={20} />}
+        </button>
+        <div className="text-lg font-semibold text-gray-800">Course Name</div>
       </div>
-      <div>box 2</div>
-      <div>box 3</div>
+      <div className="hidden md:block text-lg font-semibold text-gray-800">
+        {currentDate}
+      </div>
+      <div className="flex items-center">
+        <div className="text-lg font-semibold text-gray-800">
+          Instructor Name
+        </div>
+        <Link to="/profile">
+          <FaUser size={20} className="ml-2 text-blue-500 cursor-pointer" />
+        </Link>
+      </div>
 
       {showSidebar && (
         <div
           ref={sidebarRef}
-          className="fixed top-0 left-0 h-screen w-1/3 bg-gray-200 z-10 shadow-lg"
+          className="fixed top-0 left-0 h-screen w-1/3 bg-white z-50 shadow-lg"
           style={{ transition: "0.5s" }}
         >
           <button
@@ -54,14 +75,15 @@ export default function NavigationBar() {
               border: "none",
               float: "right",
               padding: "10px",
+              outline: "none",
             }}
             onClick={toggleSidebar}
           >
             <FaTimes size={20} />
           </button>
           <div className="p-5">
-            <p className="text-lg font-bold">Sidebar content</p>
-            <p className="mt-3">
+            <p className="text-lg font-bold text-gray-800">Sidebar content</p>
+            <p className="mt-3 text-gray-700">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
               condimentum posuere odio, vitae volutpat mauris dignissim vel.
             </p>
