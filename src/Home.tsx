d5e7fb69d58f52ecc0fe1e546e185290c880b098
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-11-07 23:10:28 +0530

    Slight Changes were made

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import CourseBox from "./Components/CourseBox";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Home Component</h1>
+    <div className="flex flex-col items-center p-6 mt-16">
+      {" "}
+      {/* Added mt-16 for margin top */}
+      <h1 className="text-3xl font-bold mb-6">Available Courses</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
+      </div>
     </div>
   );
 };
