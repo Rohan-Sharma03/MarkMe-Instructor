e90445103a5172d01b7ade049693d92538852714
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-12-02 23:38:20 +0530

    Hook to maintain login state and save some data

---
+++ src/context/useAuth.tsx
@@ -0,0 +1,67 @@
+import React, { ReactNode, createContext, useContext, useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  courses: string[];
+  number: string;
+  // Add other user-related fields as needed
+};
+
+type AuthContextType = {
+  isLoggedIn: boolean;
+  user: User | null;
+  login: (userData: User) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const storedValue = localStorage.getItem("isLoggedIn");
+    return storedValue ? JSON.parse(storedValue) : false;
+  });
+
+  const [userData, setUserData] = useState<User | null>(() => {
+    const storedUserData = localStorage.getItem("userData");
+    return storedUserData ? JSON.parse(storedUserData) : null;
+  });
+
+  const login = (userData: User) => {
+    setIsLoggedIn(true);
+    setUserData(userData);
+    console.log("Form use auth : ", userData);
+    localStorage.setItem("userData", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUserData(null);
+    localStorage.clear();
+  };
+
+  const authContextValue: AuthContextType = {
+    isLoggedIn,
+    user: userData,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
