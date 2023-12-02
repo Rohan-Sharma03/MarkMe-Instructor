6f11cecfd55e48602db8884076c4f952ceaf9ef6
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-12-02 23:39:00 +0530

    Integrated password set feature

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,45 +1,128 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, FormEvent } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function CreateAccount() {
+export default function CreateAccount(): JSX.Element {
+  const [instructorId, setInstructorId] = useState<string>("");
+  const [instructorName, setInstructorName] = useState<string>("");
+  const [instructorEmail, setInstructorEmail] = useState<string>("");
+  const [contactNumber, setContactNumber] = useState<string>("");
+  const [instructorDesignation, setInstructorDesgination] =
+    useState<string>("");
+
   const navigate = useNavigate();
 
-  const handleSignIn = () => {
-    navigate("/signIn");
+  const handleSign = () => {
+    navigate("/signIn", { replace: true });
   };
+  const handleCreateAccount = () => {
+    navigate(
+      {
+        pathname: "/credential",
+        search: createSearchParams({
+          email: instructorEmail,
+        }).toString(),
+      },
+      { replace: true }
+    );
+  };
+
   const createAccount = async () => {
     try {
-      const res = await axios.get(`${process.env.API_URL}/api/getCourses`);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/ciAccount`,
+        {
+          instructor_id: instructorId,
+          instructor_name: instructorName,
+          instructor_email: instructorEmail,
+          contact_number: contactNumber,
+          instructor_designation: instructorDesignation,
+          office_status: "NA",
+          ongoing_course: ["NA"],
+        }
+      );
+      console.log(response.data); // Log the response data if needed
+
+      // Handle success, show success message using react-toastify
+      if (response.data.status === 500) {
+        toast.error("Account exit already", {
+          position: toast.POSITION.BOTTOM_CENTER,
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeButton: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.success("Account Created", {
+          position: toast.POSITION.BOTTOM_CENTER,
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeButton: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        handleCreateAccount();
+      }
+    } catch (error) {
+      console.error("Error creating account:", error);
+      // Handle error, show error message to the user, etc.
     }
   };
 
-  useEffect(() => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      !instructorId ||
+      !instructorName ||
+      !instructorEmail ||
+      !contactNumber ||
+      !instructorDesignation
+    ) {
+      toast.error("Please fill out all fields", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeButton: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return; // Prevent further execution of the form submission
+    }
     createAccount();
-  });
+  };
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="container p-6 bg-white rounded-md shadow-md max-w-md">
-        <h2 className="text-2xl font-semibold mb-4 text-center text-green-500">
+        <h2 className="text-2xl font-semibold mb-4 text-center text-blue-500">
           Create Account
         </h2>
-        <form className="space-y-3">
+        <form
+          className="space-y-3"
+          onSubmit={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}
+        >
           <div className="flex flex-col space-y-1">
             <label
               htmlFor="instructor_id"
               className="text-sm font-medium text-gray-700"
             >
-              Instructor ID
+              Employee ID
             </label>
             <input
               id="instructor_id"
               name="instructor_id"
               type="text"
-              placeholder="Enter Instructor ID"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
+              value={instructorId}
+              onChange={(e) => setInstructorId(e.target.value)}
+              placeholder="e.g JKE00X"
+              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="flex flex-col space-y-1">
@@ -47,14 +130,16 @@ export default function CreateAccount() {
               htmlFor="instructor_name"
               className="text-sm font-medium text-gray-700"
             >
-              Instructor Name
+              Full Name
             </label>
             <input
               id="instructor_name"
               name="instructor_name"
               type="text"
-              placeholder="Enter Instructor Name"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
+              value={instructorName}
+              onChange={(e) => setInstructorName(e.target.value)}
+              placeholder="e.g Prof. Ramkrishanan"
+              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="flex flex-col space-y-1">
@@ -62,29 +147,16 @@ export default function CreateAccount() {
               htmlFor="instructor_email"
               className="text-sm font-medium text-gray-700"
             >
-              Instructor Email
+              Email Address
             </label>
             <input
               id="instructor_email"
               name="instructor_email"
               type="email"
-              placeholder="Enter Instructor Email"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
-            />
-          </div>
-          <div className="flex flex-col space-y-1">
-            <label
-              htmlFor="ongoing_courses"
-              className="text-sm font-medium text-gray-700"
-            >
-              Ongoing Courses
-            </label>
-            <input
-              id="ongoing_courses"
-              name="ongoing_courses"
-              type="text"
-              placeholder="Enter Ongoing Courses"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
+              value={instructorEmail}
+              onChange={(e) => setInstructorEmail(e.target.value)}
+              placeholder="e.g temp@example.com"
+              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="flex flex-col space-y-1">
@@ -98,28 +170,32 @@ export default function CreateAccount() {
               id="contact_number"
               name="contact_number"
               type="tel"
-              placeholder="Enter Contact Number"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
+              value={contactNumber}
+              onChange={(e) => setContactNumber(e.target.value)}
+              placeholder="e.g 8923456723"
+              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
           </div>
           <div className="flex flex-col space-y-1">
             <label
-              htmlFor="office_status"
+              htmlFor="designation"
               className="text-sm font-medium text-gray-700"
             >
-              Office Status
+              Job Title
             </label>
             <input
-              id="office_status"
-              name="office_status"
+              id="designation"
+              name="designation"
               type="text"
-              placeholder="Enter Office Status"
-              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-green-500"
+              value={instructorDesignation}
+              onChange={(e) => setInstructorDesgination(e.target.value)}
+              placeholder="e.g Associate Prof."
+              className="py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
           </div>
           <button
             type="submit"
-            className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 w-full focus:outline-none"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 w-full focus:outline-none"
           >
             Create Account
           </button>
@@ -127,13 +203,15 @@ export default function CreateAccount() {
         <div className="mt-3 text-sm text-gray-600 text-center">
           Already have an account?
           <span
-            onClick={handleSignIn}
+            onClick={handleSign}
             className="cursor-pointer text-blue-500 ml-1 hover:underline"
           >
             Sign In
           </span>
         </div>
       </div>
+      {/* Place ToastContainer at the root level */}
+      <ToastContainer />
     </div>
   );
 }
